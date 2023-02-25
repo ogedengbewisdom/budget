99e80da123030d43417a625f0d2dfb65e0c4331f
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-25 23:07:45 +0100

    rendring out logout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Home from "./Home/Home";
+import Logins from "./Logins/Logins";
 
 function App() {
   return (
     <Fragment>
       <Home />
+      <Logins />
     </Fragment>
   );
 }
