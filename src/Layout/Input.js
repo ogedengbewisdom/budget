e5178fad58458af0b94db447ea610d2b493f5025
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-26 01:20:13 +0100

    useRef hook

--- src/Layout/Input.js
+++ src/Layout/Input.js
@@ -1,13 +1,20 @@
+import React from "react"
+
 import classes from "./Input.module.css"
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
 
     return (
         <div className={classes.inputs}>
             <label htmlFor={props.input.id}>{props.label}</label>
-            <input {...props.input} onChange={props.onChange} value={props.value} />
+            <input
+                {...props.input} 
+                onChange={props.onChange} 
+                value={props.value} 
+                ref={ref}
+             />
         </div>
     )
-}
+})
 
 export default Input
