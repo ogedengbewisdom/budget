88c1fdb68e97f8afa65dd34c3d641ac442585a0b
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-28 00:35:46 +0100

    Logo sizing

--- src/Home/Logo.js
+++ src/Home/Logo.js
@@ -3,7 +3,7 @@ import classes from "./Logo.module.css"
 
 const Logo = (props) => {
     return (
-            <h2 className={classes.log}>goBudget</h2>
+            <h2 className={classes.log}><span className={classes.special}>go</span>Bud<span className={classes.special}>get</span></h2>
     )
 }
 
