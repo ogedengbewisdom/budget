18ab748a5dab4d8300645eabd9ed1e4780e474e9
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-27 21:33:55 +0100

    Adding onBlur props

--- src/Layout/Input.js
+++ src/Layout/Input.js
@@ -5,13 +5,15 @@ import classes from "./Input.module.css"
 const Input = React.forwardRef((props, ref) => {
 
     return (
-        <div className={classes.inputs}>
+        <div className={`${classes.inputs} ${props.isValid === false ? classes.invalid : ""}`}>
             <label htmlFor={props.input.id}>{props.label}</label>
             <input
                 {...props.input} 
                 onChange={props.onChange} 
                 value={props.value} 
                 ref={ref}
+                isValid= {props.isValid}
+                onBlur= {props.onBlur}
              />
         </div>
     )
