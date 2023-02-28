2428dfe4afcf7bb99a2156d05b5ed0fe695da37a
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-28 03:58:03 +0100

    Navigation Bar usecontext

--- src/Home/NavBar.js
+++ src/Home/NavBar.js
@@ -1,14 +1,18 @@
+import { useContext } from "react"
+import AuthorizeProps from "../Store/auth_context"
 import classes from "./NavBar.module.css"
 
 const NavBar = () => {
+
+    const authctx = useContext(AuthorizeProps)
     return (
         <div className={classes.mar}>
         <nav className={classes.navs}>
             <ul>
-                <li>HOME</li>
-                <li>Expenses</li>
-                <li>Chart</li>
-                <li>Logout</li>
+                {authctx.isLogin && <li><a href="/">HOME</a></li>}
+                {authctx.isLogin && <li><a href="/">Expenses</a></li>}
+                {authctx.isLogin && <li><a href="/">Chart</a></li>}
+                {authctx.isLogin && <li><button onClick={authctx.onLogout}>Logout</button></li>}
             </ul>
         </nav>
         </div>
