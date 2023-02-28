801863ba5877671f85c945426a5f09f2ec1ef78d
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-02-28 03:52:10 +0100

    use AuthorizeProps component to wrap the App

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import AuthorizeProvider from './Store/AuthorizeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+<AuthorizeProvider>
+<App />
+</AuthorizeProvider>
+);
