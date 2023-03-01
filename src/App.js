812cf82975be691e8b18b62da36014ca51da0bf0
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-01 11:20:12 +0100

    trying to fix some errors

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { Fragment, useContext } from "react";
 import Home from "./Home/Home";
 import Logins from "./Logins/Logins";
 import AuthorizeProps from "./Store/auth_context";
+import ReactDOM from "react-dom";
 
 function App() {
 
   const authctx = useContext(AuthorizeProps)
 
+  const homePortal = (props) => {
+    return <Home />
+  }
+
+  const loginsPortal = props => {
+    return {!authctx.isLogin && <Logins />}
+  }
+
   return (
-    <Fragment>
-      <Home />
-      {!authctx.isLogin && <Logins />}
+    <Fragment>  
+      {ReactDOM.createPortal(<loginsPortal />, document.getElementById("do"))}  
     </Fragment>
   );
 }
