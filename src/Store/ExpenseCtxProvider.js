46b4355a26a2831bf5610c5731edbb0cb1dd7f98
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-04 03:38:17 +0100

    Adding a dummy data array of object

---
+++ src/Store/ExpenseCtxProvider.js
@@ -0,0 +1,51 @@
+import { useReducer } from "react"
+import ExpenseCtx from "./expense_context"
+
+const dummyData= [
+    {
+        title: "Baby Dipers",
+        date: new Date ("2021-12-22"),
+        amount: 2,
+        price: 554.5
+    },
+
+    {
+        title: "Corn Flakes",
+        date: new Date ("2020-01-18"),
+        amount: 5,
+        price: 5.3
+    },
+
+    {
+        title: "Milk",
+        date: new Date ("2022-06-02"),
+        amount: 3,
+        price: 10.54
+    }
+]
+
+const expenseDataObj = {
+    item: dummyData
+}
+
+const expenseReducer = (state, action) => {
+    return expenseDataObj
+}
+const ExpenseCtxProvider = props => {
+
+    const [expenseData, dispatchExpenseData] = useReducer(expenseReducer, expenseDataObj)
+
+
+
+    const myData = {
+        expenseArray: expenseData.item,
+    }
+
+    return (
+        <ExpenseCtx.Provider value={myData}>
+            {props.children}
+        </ExpenseCtx.Provider>
+    )
+}
+
+export default ExpenseCtxProvider
