327cf2bccbf60df5d541a2fce71a41213189f733
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-04 03:41:51 +0100

    passing data via props

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -1,21 +1,17 @@
+import { useContext } from "react"
 import Card from "../Layout/Card"
 import ExpenseDate from "./ExpenseDate"
 import classes from "./ExpenseItem.module.css"
 
-const ExpenseItem = () => {
-
-    const title = "Baby Dipers"
-    const date = new Date ("2021-12-22")
-    const amount = 2
-    const price = 554.5
-
+const ExpenseItem = (props) => {
+   
     return (
         
         <Card className={classes.container}>  
-            <ExpenseDate date={date} />
-            <h2>{title}</h2>
-            <h2 className={classes.amount}>{amount}</h2>
-            <h2 className={classes.price}>${price}</h2>
+            <ExpenseDate date={props.date} />
+            <h2>{props.title}</h2>
+            <h2 className={classes.amount}>{props.amount}</h2>
+            <h2 className={classes.price}>${props.price}</h2>
         </Card>
        
     )
