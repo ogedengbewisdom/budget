5b15b9b86f44899ce41ee0ef5cd2e8985b4e1c51
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-04 03:40:53 +0100

    map method to loop through expenseitem component data

--- src/Expenses/Expenses.js
+++ src/Expenses/Expenses.js
@@ -1,14 +1,25 @@
+import { useContext } from "react"
 import Card from "../Layout/Card"
+import ExpenseCtx from "../Store/expense_context"
 import ExpenseItem from "./ExpenseItem"
 import classes from "./Expenses.module.css"
 
 const Expenses = () => {
+
+    const expCtx = useContext(ExpenseCtx)
     return (
         <Card className={classes.expenses}>
-            <ExpenseItem />
-            <ExpenseItem />
-            <ExpenseItem />
-            <ExpenseItem />
+            {expCtx.expenseArray.map(pros =>
+            <ExpenseItem 
+            key={Math.random()}
+            title={pros.title} 
+            date= {pros.date}
+            price={pros.price}
+            amount={pros.amount}
+            />
+            )}
+            
+            
         </Card>
     )
 }
