ebccde3a8e9f76ccd6e949229ac862c74de95eb4
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-07 15:44:05 +0100

    filter the expenseArray, and render alternative msg when there is nothing on the array base the on selected year

--- src/Expenses/Expenses.js
+++ src/Expenses/Expenses.js
@@ -1,16 +1,27 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import Card from "../Layout/Card"
 import ExpenseCtx from "../Store/expense_context"
+import ExpenseFilter from "./ExpenseFilter"
 import ExpenseItem from "./ExpenseItem"
 import classes from "./Expenses.module.css"
 
 const Expenses = () => {
 
+    const [filteredYear, setFilteredYear] = useState("2021")
+
+    const dropdown = (selectedYear) => {
+        setFilteredYear(selectedYear)
+    }
+
     const expCtx = useContext(ExpenseCtx)
+
+    const filteryearExpenses = expCtx.expenseArray.filter(item => item.date.getFullYear().toString() === filteredYear)
+
     return (
         <Card className={classes.expenses}>
-            {expCtx.expenseArray.map(data =>
-            <ExpenseItem 
+            <ExpenseFilter selected={filteredYear} onChangeFilter={dropdown} />
+            {filteryearExpenses.length === 0 ? <h2 className={classes.expenseText}>No Expense Found, Please Save Expense Using the AddExpense Button Above</h2> : filteryearExpenses.map(data =>
+            <ExpenseItem filteryearExpense={filteryearExpenses}
             key={Math.random().toString()}
             title={data.title} 
             date= {data.date}
